e462a27cafd136a4c3769a10f2ce93741e27b3d4
pedro <pedro-gomes17@hotmail.com>
2019-11-22 15:59:42 -0300

    Criado migration ordem e Coupon e a tabela associativa entre as duas

---
+++ database/migrations/1574447833459_order_schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OrderSchema extends Schema {
+  up () {
+    this.create('orders', (table) => {
+      table.increments()
+      table.decimal('total', 12,2).defaultTo(0.0)
+      table.integer('user_id').unsgined()
+      table.enu('status', [
+        'pending',
+        'paid',
+        'shipped',
+        'finished',
+        'cancelled'
+      ]) //Pendente, pago, enviado, finalizado, cancelado
+      table.timestamps()
+
+      table
+      .foreign('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE') 
+    })
+  }
+
+  down () {
+    this.drop('orders')
+  }
+}
+
+module.exports = OrderSchema
---
+++ database/migrations/1574448808236_coupon_order_schema.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CouponOrderSchema extends Schema {
+  up () {
+    this.create('coupon_order', (table) => {
+      table.increments()
+      table.integer('coupon_id').unsigned();
+      table.integer('order_id').unsigned();
+      table.decimal('discount', 12, 2).defaultTo(0.0);
+      table.timestamps()
+
+      table
+      .foreign('coupon_id')
+      .references('id')
+      .inTable('coupons')
+      .onDelete('CASCADE')
+      table
+      .foreign('order_id')
+      .references('id')
+      .inTable('oders')
+      .onDelete('CASCADE')
+    
+    })
+
+    
+  }
+
+  down () {
+    this.drop('coupon_orders')
+  }
+}
+
+module.exports = CouponOrderSchema
